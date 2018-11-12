54c280e8df21e9839f74bb8e0eb468a255bb4195
msanchis <miquelangelsanchis@gmail.com>
2018-11-12 11:08:33 +0100

    Update RsaCtfTool.sh
    Fix mistakes

--- install/RsaCtfTool.sh
+++ install/RsaCtfTool.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 #Download
-git clone https://github.com/Ganapati/RsaCtfTool.git
+git clone https://github.com/Ganapati/RsaCtfTool.git /opt/RsaCtfTool
 
 #Install dependencies
 sudo apt-get install libgmp3-dev libmpc-dev
@@ -9,4 +11,5 @@ sudo apt-get install libgmp3-dev libmpc-dev
 #Install python requirements
 pip install -r "requirements.txt"
 
+#Execute tool
 #python2.7 RsaCtfTool.py
